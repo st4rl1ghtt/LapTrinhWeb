c0d40a9d534a083557f91ddb1f0542759aa6f4f3
st4rl1ghtt <155014829+st4rl1ghtt@users.noreply.github.com>
2026-05-15 09:24:16 +0700

    BTVN3

---
+++ BTVN3/Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BTVN3.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BTVN3.Controllers
+{
+    public class StudentController : Controller
+    {
+        private static List<Student> danhSachSinhVien = new List<Student>();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ShowKQ(Student student)
+        {
+            if (student != null)
+            {
+                danhSachSinhVien.Add(student);
+            }
+
+            int soLuongCungNganh = danhSachSinhVien.Count(s => s.ChuyenNganh == student.ChuyenNganh);
+
+            ViewBag.MSSV = student.MSSV;
+            ViewBag.HoTen = student.HoTen;
+            ViewBag.ChuyenNganh = student.ChuyenNganh;
+            ViewBag.SoLuong = soLuongCungNganh;
+
+            return View();
+        }
+    }
+}
---
+++ BTVN3/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace BTVN3.Models
+{
+    public class Student
+    {
+        public string MSSV { get; set; }
+        public string HoTen { get; set; }
+        public double DiemTB { get; set; }
+        public string ChuyenNganh { get; set; }
+    }
+}
